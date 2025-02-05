2c69b0f45b0173b970091dc7235937ac3e75d6c2
Abhishek-patole <abhishekpatoles13@gmail.com>
2025-02-05 21:42:00 +0530

    New project added

---
+++ TailwindProps/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='bg-green-300  text-black p-4 rounded-xl'>Hello world</h1>
+   <Card userName = "Abhishek patil" post="Staff Enginner"/>
+   <Card userName = "Rohit sinde" />
+    </>
+      
+  )
+}
+
+export default App
